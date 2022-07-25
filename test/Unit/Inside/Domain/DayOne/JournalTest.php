0f7401317f4e8e644c61e2924a9f4aa7df11b97e
Andreas Möller <am@localheinz.com>
2022-07-25 21:52:31 +0200

    Enhancement: Implement Journal

---
+++ test/Unit/Inside/Domain/DayOne/JournalTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/day-one-to-obsidian-converter
+ */
+
+namespace Ergebnis\DayOneToObsidianConverter\Test\Unit\Inside\Domain\DayOne;
+
+use Ergebnis\DayOneToObsidianConverter\Inside;
+use Ergebnis\DayOneToObsidianConverter\Test;
+use PHPUnit\Framework;
+
+/**
+ * @internal
+ *
+ * @covers \Ergebnis\DayOneToObsidianConverter\Inside\Domain\DayOne\Journal
+ *
+ * @uses \Ergebnis\DayOneToObsidianConverter\Inside\Domain\Shared\BaseName
+ * @uses \Ergebnis\DayOneToObsidianConverter\Inside\Domain\Shared\Directory
+ * @uses \Ergebnis\DayOneToObsidianConverter\Inside\Domain\Shared\Extension
+ * @uses \Ergebnis\DayOneToObsidianConverter\Inside\Domain\Shared\FileName
+ * @uses \Ergebnis\DayOneToObsidianConverter\Inside\Domain\Shared\FilePath
+ */
+final class JournalTest extends Framework\TestCase
+{
+    use Test\Util\Helper;
+
+    public function testCreateReturnsJournal(): void
+    {
+        $faker = self::faker();
+
+        $filePath = Inside\Domain\Shared\FilePath::create(
+            Inside\Domain\Shared\Directory::fromString($faker->slug()),
+            Inside\Domain\Shared\FileName::fromString(\sprintf(
+                '%s.%s',
+                $faker->slug(),
+                $faker->fileExtension(),
+            )),
+        );
+
+        $journal = Inside\Domain\DayOne\Journal::create($filePath);
+
+        self::assertSame($filePath, $journal->filePath());
+    }
+}
