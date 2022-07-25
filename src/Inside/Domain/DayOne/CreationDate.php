128fa47dde8f2e60d4aceb953d564ebd1f8fee07
Andreas Möller <am@localheinz.com>
2022-07-25 21:44:06 +0200

    Enhancement: Implement CreationDate

---
+++ src/Inside/Domain/DayOne/CreationDate.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/day-one-to-obsidian-converter
+ */
+
+namespace Ergebnis\DayOneToObsidianConverter\Inside\Domain\DayOne;
+
+/**
+ * @psalm-immutable
+ */
+final class CreationDate
+{
+    private function __construct(private readonly \DateTimeImmutable $value)
+    {
+    }
+
+    public static function fromDateTimeImmutable(\DateTimeImmutable $value): self
+    {
+        return new self($value);
+    }
+
+    public function toDateTimeImmutable(): \DateTimeImmutable
+    {
+        return $this->value;
+    }
+}
---
+++ test/Unit/Inside/Domain/DayOne/CreationDateTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/day-one-to-obsidian-converter
+ */
+
+namespace Ergebnis\DayOneToObsidianConverter\Test\Unit\Inside\Domain\DayOne;
+
+use Ergebnis\DayOneToObsidianConverter\Inside;
+use Ergebnis\DayOneToObsidianConverter\Test;
+use PHPUnit\Framework;
+
+/**
+ * @internal
+ *
+ * @covers \Ergebnis\DayOneToObsidianConverter\Inside\Domain\DayOne\CreationDate
+ */
+final class CreationDateTest extends Framework\TestCase
+{
+    use Test\Util\Helper;
+
+    public function testFromDateTimeImmutableReturnsCreationDate(): void
+    {
+        $value = \DateTimeImmutable::createFromMutable(self::faker()->dateTime());
+
+        $creationDate = Inside\Domain\DayOne\CreationDate::fromDateTimeImmutable($value);
+
+        self::assertSame($value, $creationDate->toDateTimeImmutable());
+    }
+}
