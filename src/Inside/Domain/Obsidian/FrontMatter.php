befae89284f2fe8c1f23b4442a13b35d16233562
Andreas Möller <am@localheinz.com>
2022-08-21 21:02:47 +0200

    Enhancement: Implement FrontMatter

---
+++ src/Inside/Domain/Obsidian/FrontMatter.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/day-one-to-obsidian-converter
+ */
+
+namespace Ergebnis\DayOneToObsidianConverter\Inside\Domain\Obsidian;
+
+/**
+ * @psalm-immutable
+ */
+final class FrontMatter
+{
+    private function __construct(private readonly array $value)
+    {
+    }
+
+    public static function fromArray(array $value): self
+    {
+        return new self($value);
+    }
+
+    public function toArray(): array
+    {
+        return $this->value;
+    }
+}
---
+++ test/Unit/Inside/Domain/Obsidian/FrontMatterTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/day-one-to-obsidian-converter
+ */
+
+namespace Ergebnis\DayOneToObsidianConverter\Test\Unit\Inside\Domain\Obsidian;
+
+use Ergebnis\DayOneToObsidianConverter\Inside;
+use Ergebnis\DayOneToObsidianConverter\Test;
+use PHPUnit\Framework;
+
+/**
+ * @internal
+ *
+ * @covers \Ergebnis\DayOneToObsidianConverter\Inside\Domain\Obsidian\FrontMatter
+ */
+final class FrontMatterTest extends Framework\TestCase
+{
+    use Test\Util\Helper;
+
+    public function testFromArrayReturnsFrontMatter(): void
+    {
+        $faker = self::faker();
+
+        $value = \array_combine(
+            $faker->words(),
+            $faker->sentences(),
+        );
+
+        $frontMatter = Inside\Domain\Obsidian\FrontMatter::fromArray($value);
+
+        self::assertSame($value, $frontMatter->toArray());
+    }
+}
