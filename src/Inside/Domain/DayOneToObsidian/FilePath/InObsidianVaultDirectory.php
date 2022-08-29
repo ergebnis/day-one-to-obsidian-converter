5fa8d6693ec3fd16b525c5621277fb00416789b3
Andreas Möller <am@localheinz.com>
2022-08-29 13:34:38 +0200

    Enhancement: Implement InObsidianVaultDirectory note file path mapper

---
+++ src/Inside/Domain/DayOneToObsidian/FilePath/InObsidianVaultDirectory.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/day-one-to-obsidian-converter
+ */
+
+namespace Ergebnis\DayOneToObsidianConverter\Inside\Domain\DayOneToObsidian\FilePath;
+
+use Ergebnis\DayOneToObsidianConverter\Inside;
+
+final class InObsidianVaultDirectory implements ObsidianNoteFilePathMapper
+{
+    public function __construct(private readonly Inside\Domain\Shared\Directory $obsidianVaultDirectory)
+    {
+    }
+
+    public function map(Inside\Domain\DayOne\Entry $dayOneEntry): Inside\Domain\Shared\FilePath
+    {
+        return Inside\Domain\Shared\FilePath::create(
+            Inside\Domain\Shared\Directory::fromString(\sprintf(
+                '%s/%s/%s/%s',
+                $this->obsidianVaultDirectory->toString(),
+                \str_replace(
+                    ': ',
+                    '/',
+                    \urldecode($dayOneEntry->journal()->filePath()->fileName()->baseName()->toString()),
+                ),
+                $dayOneEntry->creationDate()->toDateTimeImmutable()->format('Y'),
+                $dayOneEntry->creationDate()->toDateTimeImmutable()->format('Y-m'),
+            )),
+            Inside\Domain\Shared\FileName::create(
+                Inside\Domain\Shared\BaseName::fromString($dayOneEntry->creationDate()->toDateTimeImmutable()->format('Y-m-d H.i')),
+                Inside\Domain\Shared\Extension::fromString('md'),
+            ),
+        );
+    }
+}
---
+++ test/Unit/Inside/Domain/DayOneToObsidian/FilePath/InObsidianVaultDirectoryTest.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/day-one-to-obsidian-converter
+ */
+
+namespace Ergebnis\DayOneToObsidianConverter\Test\Unit\Inside\Domain\DayOneToObsidian\FilePath;
+
+use Ergebnis\DayOneToObsidianConverter\Inside;
+use Ergebnis\DayOneToObsidianConverter\Test;
+use PHPUnit\Framework;
+
+/**
+ * @internal
+ *
+ * @covers \Ergebnis\DayOneToObsidianConverter\Inside\Domain\DayOneToObsidian\FilePath\InObsidianVaultDirectory
+ *
+ * @uses \Ergebnis\DayOneToObsidianConverter\Inside\Domain\DayOne\CreationDate
+ * @uses \Ergebnis\DayOneToObsidianConverter\Inside\Domain\DayOne\Entry
+ * @uses \Ergebnis\DayOneToObsidianConverter\Inside\Domain\DayOne\EntryIdentifier
+ * @uses \Ergebnis\DayOneToObsidianConverter\Inside\Domain\DayOne\Journal
+ * @uses \Ergebnis\DayOneToObsidianConverter\Inside\Domain\DayOne\ModifiedDate
+ * @uses \Ergebnis\DayOneToObsidianConverter\Inside\Domain\Shared\BaseName
+ * @uses \Ergebnis\DayOneToObsidianConverter\Inside\Domain\Shared\Directory
+ * @uses \Ergebnis\DayOneToObsidianConverter\Inside\Domain\Shared\Extension
+ * @uses \Ergebnis\DayOneToObsidianConverter\Inside\Domain\Shared\FileName
+ * @uses \Ergebnis\DayOneToObsidianConverter\Inside\Domain\Shared\FilePath
+ * @uses \Ergebnis\DayOneToObsidianConverter\Inside\Domain\Shared\Text
+ */
+final class InObsidianVaultDirectoryTest extends Framework\TestCase
+{
+    use Test\Util\Helper;
+
+    /**
+     * @dataProvider provideOriginalJournalBaseNameAndModifiedJournalBaseName
+     */
+    public function testMapReturnsFilePathCombingObsidianVaultDirectoryDayOneJournalNameAndRepresentationOfEntryCreationDate(
+        Inside\Domain\Shared\BaseName $originalJournalBaseName,
+        Inside\Domain\Shared\BaseName $modifiedJournalBaseName,
+    ): void {
+        $faker = self::faker();
+
+        $dayOneEntry = Inside\Domain\DayOne\Entry::create(
+            Inside\Domain\DayOne\Journal::create(Inside\Domain\Shared\FilePath::create(
+                Inside\Domain\Shared\Directory::fromString($faker->slug()),
+                Inside\Domain\Shared\FileName::create(
+                    $originalJournalBaseName,
+                    Inside\Domain\Shared\Extension::fromString($faker->fileExtension()),
+                ),
+            )),
+            Inside\Domain\DayOne\EntryIdentifier::fromString($faker->sha1()),
+            Inside\Domain\DayOne\CreationDate::fromDateTimeImmutable(\DateTimeImmutable::createFromMutable($faker->dateTime())),
+            Inside\Domain\DayOne\ModifiedDate::fromDateTimeImmutable(\DateTimeImmutable::createFromMutable($faker->dateTime())),
+            Inside\Domain\Shared\Text::fromString($faker->realText()),
+            [],
+            [],
+            [],
+        );
+
+        $obsidianVaultDirectory = Inside\Domain\Shared\Directory::fromString($faker->slug());
+
+        $attachmentFilePathGenerator = new Inside\Domain\DayOneToObsidian\FilePath\InObsidianVaultDirectory($obsidianVaultDirectory);
+
+        $obsidianNoteFilePath = $attachmentFilePathGenerator->map($dayOneEntry);
+
+        $expected = Inside\Domain\Shared\FilePath::create(
+            Inside\Domain\Shared\Directory::fromString(\sprintf(
+                '%s/%s/%s/%s',
+                $obsidianVaultDirectory->toString(),
+                $modifiedJournalBaseName->toString(),
+                $dayOneEntry->creationDate()->toDateTimeImmutable()->format('Y'),
+                $dayOneEntry->creationDate()->toDateTimeImmutable()->format('Y-m'),
+            )),
+            Inside\Domain\Shared\FileName::create(
+                Inside\Domain\Shared\BaseName::fromString($dayOneEntry->creationDate()->toDateTimeImmutable()->format('Y-m-d H.i')),
+                Inside\Domain\Shared\Extension::fromString('md'),
+            ),
+        );
+
+        self::assertEquals($expected, $obsidianNoteFilePath);
+    }
+
+    /**
+     * @return \Generator<string, array{0: Inside\Domain\Shared\BaseName, 1: Inside\Domain\Shared\BaseName}>
+     */
+    public function provideOriginalJournalBaseNameAndModifiedJournalBaseName(): \Generator
+    {
+        $values = [
+            'with-colon' => [
+                Inside\Domain\Shared\BaseName::fromString(\urlencode('Foo: Bar')),
+                Inside\Domain\Shared\BaseName::fromString('Foo/Bar'),
+            ],
+            'with-slash-forward' => [
+                Inside\Domain\Shared\BaseName::fromString(\urlencode('Foo/Bar')),
+                Inside\Domain\Shared\BaseName::fromString('Foo/Bar'),
+            ],
+        ];
+
+        foreach ($values as $key => [$originalJournalBaseName, $modifiedJournalBaseName]) {
+            yield $key => [
+                $originalJournalBaseName,
+                $modifiedJournalBaseName,
+            ];
+        }
+    }
+}
