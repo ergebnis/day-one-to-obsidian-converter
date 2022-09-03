a8ba48292c9276f8f959608e620e1afd3dbbc968
Andreas Möller <am@localheinz.com>
2022-09-03 17:44:16 +0200

    Enhancement: Implement Path

---
+++ src/Inside/Domain/Shared/Path.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/day-one-to-obsidian-converter
+ */
+
+namespace Ergebnis\DayOneToObsidianConverter\Inside\Domain\Shared;
+
+/**
+ * @psalm-immutable
+ */
+final class Path
+{
+    private function __construct(private readonly string $value)
+    {
+    }
+
+    public static function fromString(string $value): self
+    {
+        return new self($value);
+    }
+
+    public function toString(): string
+    {
+        return $this->value;
+    }
+}
---
+++ test/Unit/Inside/Domain/Shared/PathTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/day-one-to-obsidian-converter
+ */
+
+namespace Ergebnis\DayOneToObsidianConverter\Test\Unit\Inside\Domain\Shared;
+
+use Ergebnis\DayOneToObsidianConverter\Inside;
+use Ergebnis\DayOneToObsidianConverter\Test;
+use PHPUnit\Framework;
+
+/**
+ * @internal
+ *
+ * @covers \Ergebnis\DayOneToObsidianConverter\Inside\Domain\Shared\Path
+ */
+final class PathTest extends Framework\TestCase
+{
+    use Test\Util\Helper;
+
+    /**
+     * @dataProvider providePath
+     */
+    public function testFromStringReturnsPath(string $value): void
+    {
+        $path = Inside\Domain\Shared\Path::fromString($value);
+
+        self::assertSame($value, $path->toString());
+    }
+
+    /**
+     * @return \Generator<string, array{0: string}>
+     */
+    public function providePath(): \Generator
+    {
+        $faker = self::faker();
+
+        $values = [
+            'path-to-directory' => \sprintf(
+                '%s/%s',
+                $faker->slug(),
+                $faker->slug(),
+            ),
+            'path-to-file' => \sprintf(
+                '%s/%s.%s',
+                $faker->slug(),
+                $faker->slug(),
+                $faker->fileExtension(),
+            ),
+        ];
+
+        foreach ($values as $key => $value) {
+            yield $key => [
+                $value,
+            ];
+        }
+    }
+}
